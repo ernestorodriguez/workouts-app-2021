bb0ef38a2f08b8117f774d5607e724e8bd0d1e4a
Ernesto Rodriguez <ernesto.rodriguez@dowjones.com>
2021-03-03 23:24:23 -0300

    added error pages and handlers for api

--- api/middlewares/workoutsMiddlewares.ts
+++ api/middlewares/workoutsMiddlewares.ts
@@ -12,8 +12,13 @@ export default class WorkoutsMiddlewares {
     await workoutsService
       .getItem("alias", alias)
       .then((response: Record<string, unknown>) => {
-        res.status(200);
-        res.json({ itemDetail: response });
+        if (response) {
+          res.status(200);
+          res.json({ itemDetail: response });
+        } else {
+          res.status(404);
+          res.json({ error: 404, status: "Not Found" });
+        }
       })
       .catch((error: Error) => next(error));
   }
